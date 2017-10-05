5b7cd894b54cff9ca406f323a8259c1b4a6cb609
Xavi Ortigosa Cortes <xaviii1234@outlook.com>
2017-10-05 11:16:43 +0200

    Pipes

---
+++ Xarxes y SO/Signals/1Father2sons.cpp
@@ -0,0 +1,32 @@
+
+
+***	1 pipe para cada hijo	***
+
+//Codigo
+void main() 
+{
+	int pipe1[2];
+	int pipe2[2];
+
+	int ph1 pipe(pipe1);
+	int ph1 pipe(pipe2);
+
+	if (fork() == 0) 
+	{
+	
+	}
+	else if(fork()==0)
+	{
+	
+	}
+	else 
+	{
+		
+	}
+}
+
+2 hijos envian al padre
+
+1 pipe, lee pero no sabe quien a no seeeeeeeeer SIGUSR1, SIGUSR2;
+para comunicar los hijos la pipe en el padre;
+
